c27d9fed68965968d7257e731bb0632d81d50e85
Li <mentosli@outlook.com>
2021-04-26 10:37:48 -0500

    start from 3

--- Simulation_GH.py
+++ Simulation_GH.py
@@ -326,55 +326,55 @@ import sys
 
 ################ Test ###################
 
-# Setting 1
-print('======================\n Here starts Setting 1 \n======================\n\n\n')
+# # Setting 1
+# print('======================\n Here starts Setting 1 \n======================\n\n\n')
 
-file_dir = '../Simulation_data_GLMM/Setting_1/'
-file_names = os.listdir(file_dir)
-for i in range(len(file_names)):
-    try:
-        start_time = time.time()
-        df = pd.read_csv(file_dir + file_names[i], index_col=0)
-        # Load data
-        data1 = np.array(df[df['Site_ID'] == 1][var_name])
-        data2 = np.array(df[df['Site_ID'] == 2][var_name])
-        data = [data1, data2]
-        out1 = np.array(df[df['Site_ID'] == 1]['y']).reshape(500,1)
-        out2 = np.array(df[df['Site_ID'] == 2]['y']).reshape(500,1)
-        out = [out1, out2]
-        # Simulations
-        [beta, mu] = GH(k, data, out)
-        output(data, beta, truth).to_csv('../Simulation_data_GLMM/Result_GH/Setting_1_' +
-                                              file_names[i][44:], header = True)
-        print('======\n File:\n', file_names[i], '\n Completed!\n')
-        print("--- %s seconds ---" % (time.time() - start_time))
-    except:
-        pass;
+# file_dir = '../Simulation_data_GLMM/Setting_1/'
+# file_names = os.listdir(file_dir)
+# for i in range(len(file_names)):
+#     try:
+#         start_time = time.time()
+#         df = pd.read_csv(file_dir + file_names[i], index_col=0)
+#         # Load data
+#         data1 = np.array(df[df['Site_ID'] == 1][var_name])
+#         data2 = np.array(df[df['Site_ID'] == 2][var_name])
+#         data = [data1, data2]
+#         out1 = np.array(df[df['Site_ID'] == 1]['y']).reshape(500,1)
+#         out2 = np.array(df[df['Site_ID'] == 2]['y']).reshape(500,1)
+#         out = [out1, out2]
+#         # Simulations
+#         [beta, mu] = GH(k, data, out)
+#         output(data, beta, truth).to_csv('../Simulation_data_GLMM/Result_GH/Setting_1_' +
+#                                               file_names[i][44:], header = True)
+#         print('======\n File:\n', file_names[i], '\n Completed!\n')
+#         print("--- %s seconds ---" % (time.time() - start_time))
+#     except:
+#         pass;
 
-# Setting 2
-print('======================\n Here starts Setting 2 \n======================\n\n\n')
+# # Setting 2
+# print('======================\n Here starts Setting 2 \n======================\n\n\n')
 
-file_dir = '../Simulation_data_GLMM/Setting_2/'
-file_names = os.listdir(file_dir)
-for i in range(len(file_names)):
-    try:
-        start_time = time.time()
-        df = pd.read_csv(file_dir + file_names[i], index_col=0)
-        # Load data
-        data1 = np.array(df[df['Site_ID'] == 1][var_name])
-        data2 = np.array(df[df['Site_ID'] == 2][var_name])
-        data = [data1, data2]
-        out1 = np.array(df[df['Site_ID'] == 1]['y']).reshape(500,1)
-        out2 = np.array(df[df['Site_ID'] == 2]['y']).reshape(500,1)
-        out = [out1, out2]
-        # Simulations
-        [beta, mu] = GH(k, data, out)
-        output(data, beta, truth).to_csv('../Simulation_data_GLMM/Result_GH/Setting_2_' +
-                                              file_names[i][44:], header = True)
-        print('======\n File:\n', file_names[i], '\n Completed!\n')
-        print("--- %s seconds ---" % (time.time() - start_time))
-    except:
-        pass;
+# file_dir = '../Simulation_data_GLMM/Setting_2/'
+# file_names = os.listdir(file_dir)
+# for i in range(len(file_names)):
+#     try:
+#         start_time = time.time()
+#         df = pd.read_csv(file_dir + file_names[i], index_col=0)
+#         # Load data
+#         data1 = np.array(df[df['Site_ID'] == 1][var_name])
+#         data2 = np.array(df[df['Site_ID'] == 2][var_name])
+#         data = [data1, data2]
+#         out1 = np.array(df[df['Site_ID'] == 1]['y']).reshape(500,1)
+#         out2 = np.array(df[df['Site_ID'] == 2]['y']).reshape(500,1)
+#         out = [out1, out2]
+#         # Simulations
+#         [beta, mu] = GH(k, data, out)
+#         output(data, beta, truth).to_csv('../Simulation_data_GLMM/Result_GH/Setting_2_' +
+#                                               file_names[i][44:], header = True)
+#         print('======\n File:\n', file_names[i], '\n Completed!\n')
+#         print("--- %s seconds ---" % (time.time() - start_time))
+#     except:
+#         pass;
 
 # Setting 3
 print('======================\n Here starts Setting 3 \n======================\n\n\n')
